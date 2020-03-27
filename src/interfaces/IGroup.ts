9b5bee7ce714f1abb0ac47f8a2a16c8ac1099f51
TomateDeveloper <ianfe@martina.com.co>
2020-03-26 22:58:39 -0500

    Updated groupModel

--- src/interfaces/IGroup.ts
+++ src/interfaces/IGroup.ts
@@ -1,16 +1,43 @@
-import { IUserPermissions } from "./permissions/IUserPermissions";
-import { IGroupPermissions } from "./permissions/IGroupPermissions";
 import { IModel } from "./IModel";
-import { IUser } from "./IUser";
+import { IPunishmentPermissions } from "./permissions/IPunishmentPermissions";
+import { IAppealsPermissions } from "./permissions/IAppealsPermissions";
+import { IReportsPermissions } from "./permissions/IReportsPermissions";
 
 export interface IGroup extends IModel {
   name: string;
-  createdBy: IUser;
-  admin: boolean;
-  permissions: IPermissions;
+  priority: number;
+  html_color: string;
+  badge_color: string;
+  badge_link: string;
+  minecraft_flair: IMinecraftFlair[];
+  minecraft_permissions: string[];
+  staff: boolean;
+  discord_role: string;
+  web_permissions: IPermissions;
+}
+
+export interface IMinecraftFlair {
+  realm: string;
+  color: string;
+  symbol: string;
 }
 
 export interface IPermissions {
-  user: IUserPermissions;
-  group: IGroupPermissions;
+  user: {
+    manage: boolean;
+  };
+  group: {
+    manage: boolean;
+  };
+  category: {
+    manage: boolean;
+  };
+  view_ips: boolean;
+  punishment: IPunishmentPermissions;
+  appeals: IAppealsPermissions;
+  maps: {
+    manage: boolean;
+  };
+  reports: IReportsPermissions;
+  forum: any;
 }
--- src/interfaces/permissions/IGroupPermissions.ts
+++
@@ -1,7 +0,0 @@
-export interface IGroupPermissions {
-  create: boolean;
-  read: boolean;
-  update: boolean;
-  assign: boolean;
-  delete: boolean;
-}
