a28b6f8fff55a13e13345a7b0274cf41e0ed2274
Ian Castaño <ianfe@martina.com.co>
2020-04-02 09:48:03 -0500

    Updated verify loggers

--- src/services/mailerService.ts
+++ src/services/mailerService.ts
@@ -1,4 +1,5 @@
-import { Inject, Service } from "typedi";
+import {Container, Inject, Service} from "typedi";
+import {Logger} from "winston";
 import {IMailUpdateVerification, IMailVerifyRequest, IUser} from "../interfaces/IUser";
 import * as fs from "fs";
 import * as Mail from "nodemailer/lib/mailer";
@@ -8,44 +9,53 @@ import config from "../config";
 export default class MailerService {
 
   constructor(
-    @Inject('mailClient') private mailClient : Mail
+    @Inject('mailClient') private mailClient : Mail,
+    @Inject('logger') private logger: Logger
   ) {}
 
   public async mailUpdate(update: IMailUpdateVerification) {
+    try {
+      const date: Date = new Date();
+      let mail: string = fs.readFileSync("../assets/templates/update.html", {encoding: 'utf-8'});
+      mail = mail.replace("%%username%%", update.user.display);
+      mail = mail.replace("%%code%%", update.code + "");
+      mail = mail.replace("%%date%%", date.getFullYear() + "");
 
-    const date: Date = new Date();
-    let mail: string = fs.readFileSync("../assets/templates/update.html", {encoding: 'utf-8'});
-    mail = mail.replace("%%username%%", update.user.display);
-    mail = mail.replace("%%code%%", update.code + "");
-    mail = mail.replace("%%date%%", date.getFullYear() + "");
+      let mailOptions = {
+        from: config.emails.auth.user,
+        to: update.user.email,
+        subject: 'Actualización de correo',
+        html: mail
+      };
 
-    let mailOptions = {
-      from: config.emails.auth.user,
-      to: update.user.email,
-      subject: 'Actualización de correo',
-      html: mail
-    };
-
-    return await this.mailClient.sendMail(mailOptions);
+      return await this.mailClient.sendMail(mailOptions);
+    } catch (e) {
+      this.logger.error('There was an error sending update email: %o', e);
+    }
   }
 
   public async mailVerify(update: IMailVerifyRequest) {
+    try {
+      const date: Date = new Date();
+      let mail: string = fs.readFileSync("../assets/templates/verify.html", {encoding: 'utf-8'});
+      this.logger.debug(mail);
+      mail = mail.replace("%%display%%", update.user.display);
+      mail = mail.replace("%%skin%%", update.user.skin);
+      mail = mail.replace("%%link%%", update.link);
+      mail = mail.replace("%%date%%", date.getFullYear() + "");
 
-    const date: Date = new Date();
-    let mail: string = fs.readFileSync("../assets/templates/verify.html", {encoding: 'utf-8'});
-    mail = mail.replace("%%display%%", update.user.display);
-    mail = mail.replace("%%skin%%", update.user.skin);
-    mail = mail.replace("%%link%%", update.link);
-    mail = mail.replace("%%date%%", date.getFullYear() + "");
 
-    let mailOptions = {
-      from: config.emails.auth.user,
-      to: update.user.email,
-      subject: 'Verifica tu correo electrónico',
-      html: mail
-    };
+      let mailOptions = {
+        from: config.emails.auth.user,
+        to: update.user.email,
+        subject: 'Verifica tu correo electrónico',
+        html: mail
+      };
 
-    return await this.mailClient.sendMail(mailOptions);
+      return await this.mailClient.sendMail(mailOptions);
+    } catch (e) {
+      this.logger.error('There was an error sending verification email: %o', e);
+    }
   }
 
 }
