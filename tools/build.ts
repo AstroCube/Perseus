8c16435fcc3b441c1e8fef0cf848f2867a0f2df2
Ian Castaño <ianfe@martina.com.co>
2020-04-02 13:40:25 -0500

    Updated build

--- tools/build.ts
+++ tools/build.ts
@@ -2,7 +2,7 @@ import shell from 'shelljs';
 
 const buildFolder = './build/';
 
-const folders = new Set(['./src/templates']);
+const folders = new Set(['./src/services/templates']);
 
 folders.forEach((folder) => {
     shell.cp('-R', folder, buildFolder);
