9f9588bfb7edaec79e480feccabed3f31d378412
Ian Castaño <ianfe@martina.com.co>
2020-06-04 22:29:49 -0500

    Updated packages

--- src/loaders/mongoose.ts
+++ src/loaders/mongoose.ts
@@ -3,7 +3,7 @@ import { Db } from 'mongodb';
 import config from '../config';
 
 export default async (): Promise<Db> => {
-    const connection = await mongoose.connect(config.databaseURL, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false });
+    const connection = await mongoose.connect(config.databaseURL, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false });
     if (config.api.dbDebug) connection.set('debug', true);
     return connection.connection.db;
 };
