69d8ee9439ce7a4e6e0009f0fbd2972c85032922
Ian Castaño <ianfe@martina.com.co>
2020-07-01 18:05:35 -0500

    Updated shit

--- src/api/middlewares/authentication.ts
+++ src/api/middlewares/authentication.ts
@@ -1,28 +1,30 @@
 import jwt from 'express-jwt';
 import config from '../../config';
 
-const getTokenFromHeader = (optional?: boolean) => {
-  return req => {
-    try {
-      if (
-          (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Token') ||
-          (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer')
-      ) {
-        return req.headers.authorization.split(' ')[1];
-      }
-    } catch (e) {
-      console.log(e);
+const getTokenFromHeader = req => {
+  try {
+    if (
+        (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Token') ||
+        (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer')
+    ) {
+      return req.headers.authorization.split(' ')[1];
     }
-    return null;
-  };
-}
+  } catch (e) {
+    console.log(e);
+  }
+  return null;
+};
 
 function authentication(optional?: boolean) {
-  return jwt({
+
+  const middle = jwt({
     secret: config.jwtSecret,
     userProperty: 'token',
     getToken: getTokenFromHeader(optional),
   });
+
+  if (!optional) return middle;
+  return null;
 }
 
 export default authentication;
