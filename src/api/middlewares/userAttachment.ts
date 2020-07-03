ec20b4b8fcc9634d28cf002b4e0fa80ee6e4ed8c
Ian Castaño <ianfe@martina.com.co>
2020-07-02 21:14:24 -0500

    Anuel

--- src/api/middlewares/userAttachment.ts
+++ src/api/middlewares/userAttachment.ts
@@ -3,27 +3,29 @@ import { Logger } from "winston";
 import {ResponseError} from "../../interfaces/error/ResponseError";
 
 const userAttachment = (optional?: boolean) => {
-  console.log("Bebecita ahora");
   return async (req, res, next) => {
     const logger : Logger = Container.get('logger');
     try {
+      console.log("Bebecita ahora");
       if (!req.token) {
+        console.log("Bebecita not authenticated");
         req.authenticated = false;
         if (!optional) throw new ResponseError('You have not provided an authentication token.', 403);
-        return next();
+        next();
       }
       const userModel = Container.get('userModel') as Models.UserModel;
       const userRecord = await userModel.findById(req.token._id);
+      console.log("Bebecita finding model");
       if (!userRecord) return res.sendStatus(401);
       const currentUser = userRecord.toObject();
       Reflect.deleteProperty(currentUser, 'password');
       Reflect.deleteProperty(currentUser, 'salt');
       req.authenticated = true;
       req.currentUser = currentUser;
-      return next();
+      next();
     } catch (e) {
       logger.error(e);
-      return next(e);
+      next(e);
     }
   };
 }
