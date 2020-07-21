3cd3eee35b30164b495c82f3d63ef81e8f02ab9b
Ian Castaño <ianfe@martina.com.co>
2020-07-21 16:25:59 -0500

    ?

--- src/models/forum/middleware/forum.ts
+++ src/models/forum/middleware/forum.ts
@@ -3,6 +3,8 @@ import {Document} from "mongoose";
 const forumWare = {
 
     find: (doc: Document) => {
+        console.log(this.constructor);
+        console.log(doc);
         console.log(this);
     }
 
