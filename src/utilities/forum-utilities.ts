51c6eb07594055b0424171c8d9be92f6c67b9870
Ian Castaño <ianfe@martina.com.co>
2020-07-24 23:38:49 -0500

    ?

--- src/utilities/forum-utilities.ts
+++ src/utilities/forum-utilities.ts
@@ -26,6 +26,7 @@ export class ForumUtilities {
 
 
         for (const f of children.data) {
+            console.log(f);
             const holder: IForumHolder = await this.getHolder(f, user);
             if (holder !== null) holders.push(holder);
         }
