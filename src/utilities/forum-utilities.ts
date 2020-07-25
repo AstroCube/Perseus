3224d67c56b245421f45b76b2dec8321ef6f4cd6
Ian Castaño <ianfe@martina.com.co>
2020-07-24 22:49:08 -0500

    ?

--- src/utilities/forum-utilities.ts
+++ src/utilities/forum-utilities.ts
@@ -44,6 +44,9 @@ export class ForumUtilities {
 
         const topic: IPaginateResult<ITopic> =
             await this.topicService.list(query, {perPage: 10, sort: 'createdAt'});
+
+        console.log(topic);
+
         const messages: IPaginateResult<IPost> =
             await this.postService.list({topic: {$in: topic.data.map(f => f._id)}}, { perPage: 10, sort: 'createdAt'}, user);
 
