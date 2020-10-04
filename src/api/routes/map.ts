d1377c0e5abc209d6e2faa43cfacf880b7bff8e0
Ian Castaño <ianfe@martina.com.co>
2020-10-04 14:16:59 -0500

    Updated map functions rework

--- src/api/routes/map.ts
+++ src/api/routes/map.ts
@@ -30,69 +30,11 @@ export default (app: Router) => {
     async (req: Request, res: Response, next: NextFunction) => {
       try {
         const mapService: MapService = Container.get(MapService);
-        const map: IMap = await mapService.loadMap(req.body);
+        const map: IMap = await mapService.create(req.body);
         return res.json(map).status(200);
       } catch (e) {
         return next(e);
       }
     });
-
-    route.get(
-        '/:id',
-        async (req: Request, res: Response, next: NextFunction) => {
-            try {
-                const mapService: MapService = Container.get(MapService);
-                const populate = Boolean(req.query.populate) || false;
-                const map: IMap = await mapService.getMap(req.params.id, populate);
-                return res.json(map).status(200);
-            } catch (e) {
-                return next(e);
-            }
-        });
-
-    route.post(
-        '/list',
-        async (req: Request, res: Response, next: NextFunction) => {
-            try {
-                const page: number = req.query.page && req.query.page !== '-1' ? parseInt(<string>req.query.page)  :  undefined;
-                const perPage: number = req.query.perPage ? parseInt(<string>req.query.perPage) : 10;
-                const mapService: MapService = Container.get(MapService);
-                return res.json(await mapService.listMaps(req.body, {...req.query, page, perPage})).status(200);
-            } catch (e) {
-                return next(e);
-            }
-        });
-
-    route.post(
-        '/get-file',
-        celebrate({
-            body: Joi.object({
-                id: Joi.string().required(),
-                type: Joi.string().required(),
-                extension: Joi.string().required()
-            })
-        }),
-        middlewares.authentication,
-        middlewares.userAttachment,
-        async (req: Request, res: Response, next: NextFunction) => {
-            try {
-                const mapService: MapService = Container.get(MapService);
-                return res.sendFile(await mapService.getMapFile(req.body.id, req.body.type, req.body.extension, req.currentUser));
-            } catch (e) {
-                return next(e);
-            }
-        });
-
-    route.get(
-        '/get-image/:id',
-        async (req: Request, res: Response, next: NextFunction) => {
-            try {
-                const mapService: MapService = Container.get(MapService);
-                return res.sendFile(await mapService.getMapFile(req.params.id, 'images', 'png', req.currentUser));
-            } catch (e) {
-                return next(e);
-            }
-        });
-
 
 };
