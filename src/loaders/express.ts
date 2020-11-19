9e3ab10a1ed879d7049d40e8e16e6c07b8a4a875
Ian Castaño <ianfe@martina.com.co>
2020-11-19 15:05:51 -0500

    Updated bodyparser limit

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -17,7 +17,7 @@ export default ({ app }: { app: express.Application }) => {
     app.enable('trust proxy');
     app.use(cors());
     app.use(require('method-override')());
-    app.use(bodyParser.json());
+    app.use(bodyParser.json({limit: '100mb'}));
     app.use(config.api.prefix, routes());
     app.use((req, res, next) => {
         next(new ResponseError('Route not found', 404));
