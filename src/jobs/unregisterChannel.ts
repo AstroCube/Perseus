7595e422b367918cdb8d5f4d09d01c7c11385e2a
yushu@unnamed.team <yushu@unnamed.team>
2020-12-28 19:46:58 -0500

    Remove unused imports, rename Logger to logger to recognise class and instance

--- src/jobs/unregisterChannel.ts
+++ src/jobs/unregisterChannel.ts
@@ -1,21 +1,18 @@
 import { Container } from 'typedi';
 import { Logger } from "winston";
-import { IUser } from "../interfaces/IUser";
-import { Document, PaginateModel } from 'mongoose';
-import {IChannel} from "../interfaces/channel/IChannel";
 import ChannelService from "../services/channel/channelService";
 
 export default class UnregisterChannel {
   public async handler(job, done): Promise<void> {
 
-    const Logger: Logger = Container.get('logger');
+    const logger: Logger = Container.get('logger');
     const service: ChannelService = Container.get(ChannelService);
 
     try {
       await service.delete(job.attrs.data.channel);
       done();
     } catch (e) {
-      Logger.error('Error cleaning unregistered users: %o', e);
+      logger.error('Error cleaning unregistered users: %o', e);
       done(e);
     }
   }
