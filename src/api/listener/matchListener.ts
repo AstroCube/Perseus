f5809255a4b602eaea325be2077941df44c78977
Ian Castaño <ianfe@martina.com.co>
2021-04-18 17:13:46 -0500

    Fixed things

--- src/api/listener/matchListener.ts
+++ src/api/listener/matchListener.ts
@@ -17,16 +17,15 @@ export class MatchListener {
 
     constructor(
         private redisMessenger: RedisMessenger,
-        private matchService: MatchService,
         @Inject('logger') private logger : Logger
     ) {
-        this.matchService = Container.get(MatchService);
     }
 
     public assignSpectator(): void {
         this.redisMessenger.registerListener("gc-assign-spectator", async (message: ISpectatorAssignMessage) => {
             try {
-                await this.matchService.assignSpectator(message.user, message.match, message.join);
+                const service: MatchService = Container.get(MatchService);
+                await service.assignSpectator(message.user, message.match, message.join);
             } catch (e) {
                 this.logger.error('Error while assigning match spectators: %o', e);
             }
@@ -36,7 +35,8 @@ export class MatchListener {
     public assignTeams(): void {
         this.redisMessenger.registerListener("gc-assign-teams", async (message: ITeamAssignMessage) => {
             try {
-                await this.matchService.assignMatchTeams(message.teams, message.match);
+                const service: MatchService = Container.get(MatchService);
+                await service.assignMatchTeams(message.teams, message.match);
             } catch (e) {
                 this.logger.error('Error while assigning match teams %o', e);
             }
@@ -46,7 +46,8 @@ export class MatchListener {
     public unAssignPending(): void {
         this.redisMessenger.registerListener("gc-pending-unassign", async (message: IPendingUnAssignMessage) => {
             try {
-                await this.matchService.unAssignPending(message.user, message.match);
+                const service: MatchService = Container.get(MatchService);
+                await service.unAssignPending(message.user, message.match);
             } catch (e) {
                 this.logger.error('Error while unassigning pending %o', e);
             }
@@ -56,7 +57,8 @@ export class MatchListener {
     public assignPending(): void {
         this.redisMessenger.registerListener("gc-pending-assign", async (message: IPendingAssignMessage) => {
             try {
-                await this.matchService.assignPending(message.assignable, message.match);
+                const service: MatchService = Container.get(MatchService);
+                await service.assignPending(message.assignable, message.match);
             } catch (e) {
                 this.logger.error('Error while assigning pending %o', e);
             }
@@ -66,7 +68,8 @@ export class MatchListener {
     public assignVictory(): void {
         this.redisMessenger.registerListener("gc-victory-assign", async (message: IWinners) => {
             try {
-                await this.matchService.validateWinners(message.match, message.winners);
+                const service: MatchService = Container.get(MatchService);
+                await service.validateWinners(message.match, message.winners);
             } catch (e) {
                 this.logger.error('Error while assigning victory %o', e);
             }
@@ -76,7 +79,8 @@ export class MatchListener {
     public disqualify(): void {
         this.redisMessenger.registerListener("gc-disqualify", async (message: IDisqualify) => {
             try {
-                await this.matchService.disqualify(message.user, message.match);
+                const service: MatchService = Container.get(MatchService);
+                await service.disqualify(message.user, message.match);
             } catch (e) {
                 this.logger.error('Error while disquialifying match %o', e);
             }
@@ -86,7 +90,8 @@ export class MatchListener {
     public privatization(): void {
         this.redisMessenger.registerListener("gc-privatization", async (message: IPrivatization) => {
             try {
-                await this.matchService.privatize(message.requester, message.match);
+                const service: MatchService = Container.get(MatchService);
+                await service.privatize(message.requester, message.match);
             } catch (e) {
                 this.logger.error('Error while privatizing match %o', e);
             }
